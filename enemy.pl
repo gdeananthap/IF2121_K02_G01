19232f5d207c75b9228d9bdd2606d24cb1f9f525
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2020-11-18 10:32:49 +0700

    Add enemy.pl

---
+++ enemy.pl
@@ -0,0 +1,11 @@
+/** ENEMY **/
+/* Terdapat 4 jenis enemy, yaitu Slime, Goblin, Wolf, dan Boss */
+
+/** Attributes **/
+/* Setiap enemy memiliki status level, HP, Attack, dan Defense */
+/* Enemy(Name, Attack, Special, Defense, MaxHP, Level) */
+
+enemy(slime, 50, 70, 40, 80, 1).
+enemy(goblin, 70, 90, 35, 80, 1).
+enemy(wolf, 80, 100, 40, 90, 1).
+enemy(boss, 300,350, 320, 500, 1).
